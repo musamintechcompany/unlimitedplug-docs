0b70996db6d058d08f2947f0984e89340d85a948
musamintechcompany <musamintechcompany@gmail.com>
2026-02-03 14:53:20 -0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/docs/laravel-saas-starter-kit-v1', function () {
+    return view('docs.laravel-saas-starter-kit-v1');
+})->name('docs.laravel-saas-starter-kit-v1');
