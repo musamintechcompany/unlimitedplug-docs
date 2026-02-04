f4b4503d1bbf757cacb14fb2577497b3591095b5
musamintechcompany <musamintechcompany@gmail.com>
2026-02-03 17:14:22 -0800

    Make docs default homepage

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('docs.laravel-saas-starter-kit-v1');
 });
+
+// Route::get('/welcome', function () {
+//     return view('welcome');
+// });
 
 Route::get('/docs/laravel-saas-starter-kit-v1', function () {
     return view('docs.laravel-saas-starter-kit-v1');
